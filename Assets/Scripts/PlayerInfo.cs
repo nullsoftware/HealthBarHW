c12e90e73858021ea5dea629498709871b2e720f
nullsoftware <tumyrthebot@gmail.com>
2021-10-21 02:39:45 +0300

    Added requared component.

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Collider2D))]
 public class PlayerInfo : MonoBehaviour
 {
     [SerializeField] private int _health = 100;
